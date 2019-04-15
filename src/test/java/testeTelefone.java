e5b633ec756155d451cc0010482e963461e984fb
Anderson Gaspar <andersongasparm@gmail.com>
2019-04-15 15:19:13 -0300

    Classe testeTelefone, iniciando

--- src/test/java/testeTelefone.java
+++ src/test/java/testeTelefone.java
@@ -13,6 +13,20 @@ public class testeTelefone {
         assertEquals("Erro no addTelefone", true, telefone.add("residencial", "32420369"));
         assertEquals("Erro no addTelefone", false, telefone.add("residencial", "32420369"));
         assertEquals("Erro no addTelefone", true, telefone.add("comercial", "32420399"));
+        assertEquals("Erro no addTelefone", false, telefone.add("comercial", "1234567890123"));
+        assertEquals("Erro no addTelefone", false, telefone.add("", "32420399"));
+        assertEquals("Erro no addTelefone", false, telefone.add("residencial", ""));
+    }
+
+    public void removeTelefone(){
+
+        Telefone telefone = new Telefone();
+
+        assertEquals("Erro no removeTelefone", true, telefone.add("residencial", "32420369"));
+    }
+
+    public void updateremoveTelefone(){
+
     }
 
 
