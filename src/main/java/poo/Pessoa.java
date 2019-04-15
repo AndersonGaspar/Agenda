6678a02fc1ff5b3b8dd502a32d0d881cb16a492d
Anderson Gaspar <andersongasparm@gmail.com>
2019-04-15 14:31:16 -0300

    Classe Pessoa criada, sem implementação dos metodos.

--- src/main/java/poo/Pessoa.java
+++ src/main/java/poo/Pessoa.java
@@ -1,5 +1,53 @@
 package poo;
 
-public class Pessoa {
+import java.time.LocalDate;
 
+public class Pessoa {
+    private String nome;
+    private String sobreNome;
+    private LocalDate dataNasc;
+    private Telefone telefones;
+    private Email emails;
+
+    public Pessoa(String n, String s, LocalDate d){
+        this.nome = n;
+        this.sobreNome = s;
+        this.dataNasc = d;
+    }
+
+    public boolean addTelefone(String r, String n){
+        return true;
+    }
+
+    public boolean addEmail(String r, String n){
+        return true;
+    }
+
+    public boolean removeTelefone(String r){
+        return true;
+    }
+
+    public boolean removeEmail(String r){
+
+    }
+
+    public boolean updateTelefone(String r, String n){
+
+    }
+
+    public boolean updateEmail(String r, String n){
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", dataNasc=" + dataNasc +
+                ", telefones=" + telefones +
+                ", emails=" + emails +
+                '}';
+    }
 }
